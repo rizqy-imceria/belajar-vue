ecbc50d6a1264e0cd10a3cf11a72f9ddd06aa505
rizqy.imceria <rizqy@imceria.com>
2022-09-09 09:15:31 +0700

    learn two way bindings props

---
+++ components/SelectWithProps.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+const selectedValue = ref(1)
+</script>
+
+<template>
+  <div>
+    <div>{{ selectedValue }}</div>
+    <select v-model="selectedValue">
+      <option value="1">
+        1
+      </option>
+      <option value="2">
+        2
+      </option>
+      <option value="3">
+        3
+      </option>
+    </select>
+  </div>
+</template>
---
+++ pages/two-ways-props.vue
@@ -0,0 +1,10 @@
+<script setup>
+import SelectWithProps from '@/components/SelectWithProps.vue'
+
+</script>
+
+<template>
+  <div style="padding: 40px;">
+    <select-with-props />
+  </div>
+</template>
