a656a679ec8873d5ae7a148a90cc8f0b45a7902e
Rizqy <rizqy.nurhaqy@gmail.com>
2022-09-11 12:58:11 +0700

    update modal transition

--- pages/modal-transition.vue
+++ pages/modal-transition.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-300 min-h-screen flex justify-center items-center">
+  <div :class="[showModal ? 'bg-[#3e3e4198]' : 'bg-gray-300', 'min-h-screen flex justify-center items-center']">
     <!-- button -->
     <button class="py-2 px-4 bg-blue-600 text-white rounded-sm text-sm" @click="showModal = !showModal">
       Open Modal
@@ -7,7 +7,7 @@
 
     <!-- modal overlay -->
     <transition name="fade">
-      <div v-if="showModal" class="absolute inset-0 bg-gray-700 top-0 flex items-center justify-center" @click="showModal = !showModal">
+      <div v-if="showModal" class="absolute inset-0 top-0 flex items-center justify-center scale-1" @click="showModal = !showModal">
         <!-- modal -->
         <div class="bg-white p-8 rounded w-[300px]">
           <p class="mb-3 leading-6 text-center text-gray-500">
@@ -40,14 +40,24 @@ export default {
 <style>
 .fade-enter {
   opacity: 0;
+  transform: scale(0);
+  /* transform: scale(1); */
 }
 
-.fade-enter-active,
+.fade-enter-active {
+  transition: all 0.3s ease;
+}
+
 .fade-leave-active {
-  transition: opacity 100ms ease-out;
+  /* transition: opacity 0.3s ease-out; */
+  transition: all 0.3s ease-in-out;
 }
 
 .fade-leave-to {
   opacity: 0;
+  transform: scale(0)
+  /* transform: scale(0); */
+  /* transform: translate3d(0); */
+  /* transform: translateX(10px); */
 }
 </style>
