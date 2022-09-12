00a1df976a387161acf6a95ea292790cdb318809
rizqy.imceria <rizqy@imceria.com>
2022-09-12 08:35:33 +0700

    select child

---
+++ components/SelectComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>selected in child: {{ selectedInChild }}</div>
+    <select v-model="selectedInChild" @change="coy($event)">
+      <option value="test">
+        test
+      </option>
+      <option value="tost">
+        tost
+      </option>
+      <option value="tist">
+        tist
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['selected'],
+  data () {
+    return {
+      selectedInChild: this.selected
+    }
+  },
+  methods: {
+    coy (event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
---
+++ pages/learn-vue.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>learn vue</div>
+    <SelectComponent v-model="selected" />
+    <div>
+      <p>selected value:</p>
+      {{ selected }}
+    </div>
+  </div>
+</template>
+<script>
+import SelectComponent from '@/components/SelectComponent.vue'
+export default {
+  components: {
+    SelectComponent
+  },
+  data () {
+    return {
+      selected: 'test'
+    }
+  }
+}
+</script>
