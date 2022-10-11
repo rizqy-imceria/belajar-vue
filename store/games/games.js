06c6baf60c404ac041b25eed886bac5ecb697e40
rizqy.imceria <rizqy@imceria.com>
2022-10-11 10:14:09 +0700

    fix error in store/games

--- store/games/games.js
+++ store/games/games.js
@@ -1,19 +1,20 @@
-const state = () => games
+// /* eslint no-undef: "off" */
+// const state = () => games
 
-const getters = {
-  bestGames (state) {
-    return state.filter(({ rating }) => {
-      return rating === 9
-    })
-  },
-  playstationfour (state) {
-    return state.filter(({ console }) => console === 'Playstation 4')
-  },
-  consoleType (state) {
-    return (consoleName) => {
-      return state.filter(({ price }) => price === 15.30)
-    }
-  }
-}
+// const getters = {
+//   bestGames (state) {
+//     return state.filter(({ rating }) => {
+//       return rating === 9
+//     })
+//   },
+//   playstationfour (state) {
+//     return state.filter(({ console }) => console === 'Playstation 4')
+//   },
+//   consoleType (state) {
+//     return (consoleName) => {
+//       return state.filter(({ price }) => price === 15.30)
+//     }
+//   }
+// }
 
-export default { state, getters }
+// export default { state, getters }
