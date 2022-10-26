fbd64ba85486c83314361a4570ce24c0ac9d7171
rizqy.imceria <rizqy@imceria.com>
2022-10-26 14:32:51 +0700

    refactor test

--- components/test/Admin.spec.js
+++ components/test/Admin.spec.js
@@ -1,25 +1,23 @@
 import { mount } from '@vue/test-utils'
 import Admin from '@/components/Admin.vue'
 
+const factory = (props) => {
+  return mount(Admin, {
+    propsData: {
+      ...props
+    }
+  })
+}
+
 describe('Test Component Admin.vue', () => {
   it('display not authorized', () => {
-    const wrapper = mount(Admin)
-
-    console.log(wrapper.text())
+    const wrapper = factory()
 
     expect(wrapper.text()).toBe('Not Authorized')
   })
 
   it('display authorized', () => {
-    const wrapper = mount(Admin, {
-      propsData: {
-        isAdmin: true
-      }
-    })
-
-    // mount(Admin, { props: {}})
-
-    console.log(wrapper.text())
+    const wrapper = factory({ isAdmin: true })
 
     expect(wrapper.text()).toBe('Admin Privileges')
   })
