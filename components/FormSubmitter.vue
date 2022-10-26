62cb3f2a2cf037f84531e33614b7acae1bb8a13b
rizqy.imceria <rizqy@imceria.com>
2022-10-26 15:55:00 +0700

    learn unit testing

---
+++ components/FormSubmitter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <form @submit.prevent="handleSubmit">
+      <input v-model="username" placeholder="username" data-username>
+      <input type="submit">
+    </form>
+
+    <div v-if="submitted && username !== ''" class="message">
+      Thank you for your submission, {{ username }}
+    </div>
+  </div>
+</template>
+<script>
+import { ref } from 'vue'
+export default {
+  setup () {
+    const username = ref('')
+    const submitted = ref(false)
+
+    function handleSubmit () {
+      submitted.value = true
+    }
+
+    return { username, submitted, handleSubmit }
+  }
+}
+</script>
---
+++ components/test/FormSubmitter.spec.js
@@ -0,0 +1,27 @@
+import { mount } from '@vue/test-utils'
+import FormSubmitter from '@/components/FormSubmitter.vue'
+
+const factory = (props) => {
+  return mount(FormSubmitter, {
+    propsData: {
+      ...props
+    }
+  })
+}
+
+describe('FormSubmitter', () => {
+  it('reveals a notification when submitted', async () => {
+    const wrapper = factory()
+
+    await wrapper.find('[data-username]').setValue('alice')
+    await wrapper.find('form').trigger('submit.prevent')
+
+    expect(wrapper.find('.message').text()).toBe('Thank you for your submission, alice')
+  })
+
+  it('notification doesnt show on submitted when data-username empty', async () => {
+    const wrapper = factory()
+
+    expect(wrapper.contains('.message')).toBe(false)
+  })
+})
