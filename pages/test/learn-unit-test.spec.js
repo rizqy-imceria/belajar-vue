31a4882162c10d573b0b502d4c133fa298f8f26e
rizqy.imceria <rizqy@imceria.com>
2022-11-04 10:27:15 +0700

    refactor unit testing

--- pages/test/learn-unit-test.spec.js
+++ pages/test/learn-unit-test.spec.js
@@ -1,21 +1,24 @@
 import { mount } from '@vue/test-utils'
 import LearnUnitTest from '../learn-unit-test.vue'
 
+const factory = (propsData) => {
+  return mount(LearnUnitTest, propsData)
+}
 describe('learn-unit-test.vue', () => {
   it('renders default', () => {
-    const wrapper = mount(LearnUnitTest)
+    const wrapper = factory()
 
     expect(wrapper.html().includes('hanya tampil di input 1 dan 2')).toBe(true)
   })
 
   it('renders with no props', () => {
-    const wrapper = mount(LearnUnitTest)
+    const wrapper = factory()
 
     expect(wrapper.html().includes('props foo: foo')).toBe(true)
   })
 
   it('renders with props', () => {
-    const wrapper = mount(LearnUnitTest, {
+    const wrapper = factory({
       propsData: {
         foo: 'cuy'
       }
