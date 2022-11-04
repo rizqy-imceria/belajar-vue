99e1544af042016258baf8a7063efe9c93b1484d
rizqy.imceria <rizqy@imceria.com>
2022-11-04 09:52:50 +0700

    learn unit test

---
+++ pages/learn-unit-test.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>
+      learn unit test
+    </p>
+    <p>selected: {{ selected }}</p>
+    <select v-model="selected" name="coy">
+      <option value="1">
+        input satu
+      </option>
+      <option value="2">
+        input dua
+      </option>
+      <option value="3">
+        input tiga
+      </option>
+      <option value="4">
+        input empat
+      </option>
+    </select>
+    <div v-show="selected == 1 || selected == 2">
+      hanya tampil di input 1 dan 2
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      selected: 1
+    }
+  }
+}
+</script>
